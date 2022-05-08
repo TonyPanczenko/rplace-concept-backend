17f795bb167796e7ce094c67759e28ea6dc6e47a
Tony Panczenko <tony.panczenko@gmail.com>
2022-05-08 18:22:54 +0100

    add api gateway, lambda, dynamodb to stack

--- bin/rplace-concept-backend.js
+++ bin/rplace-concept-backend.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const cdk = require('aws-cdk-lib');
 const { RplaceConceptBackendStack } = require('../lib/rplace-concept-backend-stack');
 
 const app = new cdk.App();
 new RplaceConceptBackendStack(app, 'RplaceConceptBackendStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION, 
+  },
 });
