a5b0deda56f85b53825c31ca51fe3b216baf9166
Tony Panczenko <tony.panczenko@gmail.com>
2022-05-09 17:27:25 +0100

    add rw permissions to lambda

--- bin/rplace-concept-backend.js
+++ bin/rplace-concept-backend.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const cdk = require('aws-cdk-lib');
-const { RplaceConceptBackendStack } = require('../lib/rplace-concept-backend-stack');
+const { BackendStack } = require('../lib/backend-stack');
 
 const app = new cdk.App();
-new RplaceConceptBackendStack(app, 'RplaceConceptBackendStack', {
+new BackendStack(app, 'BackendStack', {
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
     region: process.env.CDK_DEFAULT_REGION, 
